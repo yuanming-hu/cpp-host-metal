69c4b3b7c7ef19ad0edb3baddcfd564d16c28ed7
Ye Kuang <yekuang.ky@gmail.com>
2020-01-21 13:54:54 +0800

    We can run Metal in CPP now!

--- main.cpp
+++ main.cpp
@@ -17,12 +17,23 @@
 #include <objc/message.h>
 #include <objc/objc.h>
 #include <objc/runtime.h>
+#include <string>
 
 extern "C" {
+void NSLog(id /* NSString * */ format, ...);
 id MTLCreateSystemDefaultDevice();
 }
 
 namespace {
+
+//  Need to have \n at the end, otherwise the compiled library is garbage...
+const char *kernel_src =
+    "#include <metal_stdlib>\n"
+    "using namespace metal;\n"
+    "kernel void add1(device int* data [[ buffer(0) ]],\n"
+    "                 const uint tid [[thread_position_in_grid]]) {\n"
+    "    data[tid] += 42;\n"
+    "}\n";
 
 template <typename O, typename C = id, typename... Args>
 C call(O *i, const char *select, Args... args) {
@@ -42,7 +53,9 @@ template <typename O> void release_ns_object(O *obj) {
   call(reinterpret_cast<id>(obj), "release");
 }
 
-struct NSString;
+constexpr int kNSUTF8StringEncoding = 4;
+
+using NSString = objc_object;
 struct MTLDevice;
 struct MTLLibrary;
 struct MTLComputePipelineState;
@@ -77,7 +90,7 @@ NSString *wrap_string_as_ns_string(const char *str, size_t len) {
   id ns_string = clscall("NSString", "alloc");
   return reinterpret_cast<NSString *>(
       call(ns_string, "initWithBytesNoCopy:length:encoding:freeWhenDone:", str,
-           len, 4, false));
+           len, kNSUTF8StringEncoding, false));
 }
 
 MTLLibrary *new_library_with_source(MTLDevice *device, const char *source,
@@ -122,9 +135,45 @@ void end_encoding(MTLComputeCommandEncoder *encoder) {
     call(encoder, "endEncoding");
 }
 
-void set_buffer(MTLComputeCommandEncoder *encoder, MTLBuffer *buffer,
-                size_t offset, size_t index) {
+MTLBuffer *new_mtl_buffer(MTLDevice *device, size_t length) {
+  id buffer = call(device, "newBufferWithLength:options:", length, 0 /* MTLResourceCPUCacheModeDefaultCache | MTLResourceStorageModeShared */);
+  return reinterpret_cast<MTLBuffer *>(buffer);
+}
+
+void set_mtl_buffer(MTLComputeCommandEncoder *encoder, MTLBuffer *buffer,
+                    size_t offset, size_t index) {
   call(encoder, "setBuffer:offset:atIndex:", buffer, offset, index);
+}
+
+void dispatch_threadgroups(MTLComputeCommandEncoder *encoder, int32_t blocks_x,
+                           int32_t blocks_y, int32_t blocks_z,
+                           int32_t threads_x, int32_t threads_y,
+                           int32_t threads_z) {
+  struct MTLSize {
+    uint64_t width;
+    uint64_t height;
+    uint64_t depth;
+  };
+
+  MTLSize threadgroups_per_grid;
+  threadgroups_per_grid.width = blocks_x;
+  threadgroups_per_grid.height = blocks_y;
+  threadgroups_per_grid.depth = blocks_z;
+
+  MTLSize threads_per_threadgroup;
+  threads_per_threadgroup.width = threads_x;
+  threads_per_threadgroup.height = threads_y;
+  threads_per_threadgroup.depth = threads_z;
+
+  call(encoder,
+       "dispatchThreadgroups:threadsPerThreadgroup:", threadgroups_per_grid,
+       threads_per_threadgroup);
+}
+
+// 1D
+void dispatch_threadgroups(MTLComputeCommandEncoder *encoder, int32_t blocks_x,
+                           int32_t threads_x) {
+  dispatch_threadgroups(encoder, blocks_x, 1, 1, threads_x, 1, 1);
 }
 
 void commit_command_buffer(MTLCommandBuffer *cmd_buffer) {
@@ -135,17 +184,67 @@ void wait_until_completed(MTLCommandBuffer *cmd_buffer) {
   call(cmd_buffer, "waitUntilCompleted");
 }
 
+void *mtl_buffer_contents(MTLBuffer *buffer) {
+  return call(buffer, "contents");
+}
+
 } // namespace
 
 int main() {
-  MTLDevice* dev = mtl_create_system_default_device();
-  std::cout << "mtl_device=" << dev << "\n";
-  MTLCommandQueue* cmd_queue = new_command_queue(dev);
+  MTLDevice *device = mtl_create_system_default_device();
+  std::cout << "mtl_device=" << device << "\n";
+
+  const std::string kernel_src_str(kernel_src);
+  MTLLibrary *kernel_lib = new_library_with_source(
+      device, kernel_src_str.data(), kernel_src_str.size());
+  std::cout << "kernel_lib=" << kernel_lib << "\n";
+
+  const std::string kernel_func_str("add1");
+  MTLFunction *kernel_func = new_function_with_name(
+      kernel_lib, kernel_func_str.data(), kernel_func_str.size());
+  std::cout << "kernel_func=" << kernel_func << "\n";
+
+  MTLComputePipelineState *pipeline_state =
+      new_compute_pipeline_state_with_function(device, kernel_func);
+  std::cout << "pipeline_state=" << pipeline_state << "\n";
+
+  constexpr size_t kBufferLen = 128;
+  constexpr size_t kBufferBytesLen = sizeof(int32_t) * kBufferLen;
+  MTLBuffer *mtl_buffer = new_mtl_buffer(device, kBufferBytesLen);
+  std::cout << "mtl_buffer=" << mtl_buffer << "\n";
+
+  MTLCommandQueue *cmd_queue = new_command_queue(device);
   std::cout << "cmd_queue=" << cmd_queue << "\n";
   MTLCommandBuffer* cmd_buffer = new_command_buffer(cmd_queue);
   std::cout << "cmd_buffer=" << cmd_buffer << "\n";
 
   MTLComputeCommandEncoder* encoder = new_compute_command_encoder(cmd_buffer);
   std::cout << "compute encoder=" << encoder << "\n";
+
+  set_compute_pipeline_state(encoder, pipeline_state);
+  std::cout << "set_compute_pipeline_state done\n";
+
+  set_mtl_buffer(encoder, mtl_buffer, /*offset=*/0, /*index=*/0);
+  std::cout << "set_mtl_buffer done\n";
+
+  dispatch_threadgroups(encoder, kBufferLen, 1);
+  std::cout << "dispatch_threadgroups done\n";
+
+  end_encoding(encoder);
+  std::cout << "end_encoding done\n";
+
+  commit_command_buffer(cmd_buffer);
+  std::cout << "commit_command_buffer done\n";
+
+  wait_until_completed(cmd_buffer);
+  std::cout << "wait_until_completed done\n";
+
+  int32_t *buffer_contents =
+      reinterpret_cast<int32_t *>(mtl_buffer_contents(mtl_buffer));
+  
+  for (int i = 0; i < kBufferLen; ++i) {
+    std::cout << "mtl_buffer[" << i << "]=" << buffer_contents[i] << "\n";
+  }
+
   return 0;
 }
